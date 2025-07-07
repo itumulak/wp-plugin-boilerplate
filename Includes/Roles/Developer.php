27b64998bfb796ad4368b87f95af40341b09a556
Ian Tumulak <edden87@gmail.com>
2025-07-07 19:46:03 +0800

    Fix formating

--- Includes/Roles/Developer.php
+++ Includes/Roles/Developer.php
@@ -2,42 +2,42 @@
 namespace Itumulak\Includes\Roles;
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 } // Exit if accessed directly
 
 use Itumulak\Includes\Interfaces\Role;
 
 class Developer implements Role {
-    private string $role = 'developer';
-    private string $display_name = 'Developer';
+	private string $role         = 'developer';
+	private string $display_name = 'Developer';
 
-    public function get_role(): string {
-        return $this->role;
-    }
+	public function get_role(): string {
+		return $this->role;
+	}
 
-    public function get_display_name(): string {
-        return $this->display_name;
-    }
+	public function get_display_name(): string {
+		return $this->display_name;
+	}
 
-    public function get_capabilities(): array {
-        return array(
-            'switch_themes',
-            'edit_theme_options',
-            'install_themes',
-            'update_themes',
-            'delete_themes',
-            'activate_plugins',
-            'install_plugins',
-            'update_plugins',
-            'delete_plugins',
-            'edit_plugins',
-            'manage_options',
-            'edit_theme_options',
-            'list_users',
-            'edit_users',
-            'create_users',
-            'delete_users',
-            'promote_users'
-        );
-    }
+	public function get_capabilities(): array {
+		return array(
+			'switch_themes',
+			'edit_theme_options',
+			'install_themes',
+			'update_themes',
+			'delete_themes',
+			'activate_plugins',
+			'install_plugins',
+			'update_plugins',
+			'delete_plugins',
+			'edit_plugins',
+			'manage_options',
+			'edit_theme_options',
+			'list_users',
+			'edit_users',
+			'create_users',
+			'delete_users',
+			'promote_users',
+		);
+	}
 }
--- Includes/Roles/RoleLoader.php
+++ Includes/Roles/RoleLoader.php
@@ -2,33 +2,33 @@
 namespace Itumulak\Includes\Roles;
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 } // Exit if accessed directly
 
 use Itumulak\Includes\Interfaces\Loader;
 use Itumulak\Includes\Roles\Developer;
 
 class RoleLoader implements Loader {
-    private array $roles;
+	private array $roles;
 
-    public function __construct() {
-        $this->load();
-    }
+	public function __construct() {
+		$this->load();
+	}
 
-    public function init(): void {
-        add_action( 'init', array( $this, 'register' ) );
-    }
+	public function init(): void {
+		add_action( 'init', array( $this, 'register' ) );
+	}
 
-    public function register(): void {
-        foreach ( $this->roles as $role ) {
-            $instance = new $role();
-            add_role( $instance->get_role(), $instance->get_display_name(), $instance->get_capabilities() );
-        }
-    }
+	public function register(): void {
+		foreach ( $this->roles as $role ) {
+			$instance = new $role();
+			add_role( $instance->get_role(), $instance->get_display_name(), $instance->get_capabilities() );
+		}
+	}
 
-    public function load(): void {
-        $this->roles = array(
-            Developer::class
-        );
-    }
+	public function load(): void {
+		$this->roles = array(
+			Developer::class,
+		);
+	}
 }
