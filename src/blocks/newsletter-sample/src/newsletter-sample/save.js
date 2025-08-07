6b559255154045a4716ff62ccb3ee0d2c56e5bca
Ian Tumulak <edden87@gmail.com>
2025-08-07 15:46:58 +0800

    Updated save.js

--- src/blocks/newsletter-sample/src/newsletter-sample/save.js
+++ src/blocks/newsletter-sample/src/newsletter-sample/save.js
@@ -1,24 +1,23 @@
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
+import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
 
-/**
- * The save function defines the way in which the different attributes should
- * be combined into the final markup, which is then serialized by the block
- * editor into `post_content`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#save
- *
- * @return {Element} Element to render.
- */
-export default function save() {
+export default function save({attributes}) {
+	const { title, showTitle, description, showDescription } = attributes;
+
 	return (
-		<p { ...useBlockProps.save() }>
-			{ 'Newsletter Sample – hello from the saved content!' }
-		</p>
+		<div { ...useBlockProps.save() }>
+			{ showTitle && <h2>{ title }</h2> }
+			{ showDescription && <p>{ description }</p> }
+			<form>
+				<input
+					type="email"
+					placeholder={__('Email', 'newsletter-sample')}
+					required
+				/>
+				<button type="submit">
+					{ __('Subscribe', 'newsletter-sample') }
+				</button>
+			</form>
+		</div>
 	);
 }
